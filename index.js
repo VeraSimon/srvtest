2d023dd1af58d6c3a52a0918111bef8c38f90f64
Vera Simon <vera.simon01@gmail.com>
2018-10-31 09:46:46 -0700

    ghjkl

--- index.js
+++ index.js
@@ -2,7 +2,5 @@ require('dotenv').config();
 
 const server = require('./api/server.js');
 
-const port = process.env.PORT || 8000;
-const instance = server.listen(port, () =>
-	console.log(`\n=== Server running on port: ${instance.address().port} ===\n`)
-);
+const port = process.env.PORT || 8080;
+const instance = server.listen(port, () => console.log(`\n=== Server running on port: ${instance.address().port} ===\n`));
